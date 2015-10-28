752caef66ec1406b4b2bec9eb9d8d3e2fe349001
Sinisa Grubor <sinisa.grubor@gmail.com>
2015-10-28 16:58:57 +0100

    cleaning photobooth api

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 //Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
-var ECT = require('ect');
-var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' });
 var responseTime = require('response-time');
-
 
 //Express
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.engine('ect', ectRenderer.render);
-app.set('view engine', 'ect');
 
 //log request stats
 app.use(responseTime(function(req, res, time) {
@@ -24,8 +19,6 @@ app.use(responseTime(function(req, res, time) {
 
 //Routes
 app.use('/api', require('./routes/api'));
-app.use('/share', require('./routes/share'));
-app.use('/photobooth', require('./routes/photobooth'));
 
 // Start server
 app.listen(3000);
